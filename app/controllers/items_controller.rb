7e4d1e725e7a38459d250957a3daf069173a96bb
kei1201 <k16180339@icloud.com>
2021-08-28 18:51:59 +0900

    修正依頼３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_root_path,only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -24,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -36,6 +36,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id != @item.user.id
+  end
 
   def set_item
     @item = Item.find(params[:id])
