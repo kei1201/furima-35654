0cb81839016fc3699d060167cd7fcc397c342aab
kei1201 <k16180339@icloud.com>
2021-09-01 19:37:20 +0900

    エラーメッセージの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,42 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
+    @item_order = Order.new
   end
 
   def create
-
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
+  end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id == @item.user.id || @item.order != nil
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
 end
