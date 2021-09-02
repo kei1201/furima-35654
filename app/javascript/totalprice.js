9df95abbfa303ccddb7fc882ff76f0d5cfe58832
kei1201 <k16180339@icloud.com>
2021-09-02 10:52:14 +0900

    売却済みでも編集削除できないようにする

--- app/javascript/totalprice.js
+++ app/javascript/totalprice.js
@@ -1,14 +1,10 @@
-function totalprice() {
-  const itemPrice = document.getElementById("item-price");
-    itemPrice.addEventListener('change', () => {
-      const price = itemPrice.value;
-      const addTaxPrice = document.getElementById("add-tax-price");
-      const profit = document.getElementById("profit");
-      const taxPrice = price * 0.1;
-      const saleProfit = price - taxPrice;
-      
-      addTaxPrice.innerHTML = taxPrice;
-      profit.innerHTML = saleProfit; 
-  });
-}
-window.addEventListener("load", totalprice);
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  const ProfitDom = document.getElementById("profit");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
+});
--- spec/factories/item_orders.rb
+++ spec/factories/item_orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item_order do
-    token          {'tok_abcdefghijk00000000000000000'}
+    token          {'tok_abcdefghijk000010000000000000'}
     postal_code    {'123-4567'}
     prefecture_id  {3}
     city_name      {'福岡市'}
