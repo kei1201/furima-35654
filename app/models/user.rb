3eae59f62643affe13c8fbe58b211198c38631e0
kei1201 <k16180339@icloud.com>
2021-09-05 19:00:53 +0900

    パスワード修正と金額修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User < ApplicationRecord
   has_many :orders
   
   with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
     validates :nickname
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,}+\z/
-    validates :password, format: { with: VALID_PASSWORD_REGEX}
-    validates :last_name, :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-    validates :kana_last, :kana_first, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
+    validates :last_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+    validates :first_name,format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+    validates :kana_last, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
+    validates :kana_first, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
     validates :birthday
   end
 end
