fa85fb00460bcaa7c833e38d49ae4c3e859bfb1f
kei1201 <k16180339@icloud.com>
2021-09-05 16:51:22 +0900

    Basic認証とS3投稿

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
+  private
 
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last, :kana_first, :birthday])
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ratio' && password == '16180'
+    end
+  end
 end
